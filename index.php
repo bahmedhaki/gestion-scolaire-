f8c414d0304ef665defa16fe57c0da2f32abf8ab
Bourasmoh <bourasha1997@gmail.com>
2018-04-03 23:17:13 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,70 @@
-
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html>
-    <head>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
 
-    </head>
+input[type=text], input[type=password] {
+    width: 50%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+button {
+    text-align: center;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 50%;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+}
+
+img.avatar {
+    width: 20%;
+    border-radius: 50%;
+}
+
+.container {
+    text-align: center;
+    padding: 16px;
+}
+
+</style>
+</head>
 <body>
-<form method="POST" action="valider.php" >
-    login:<input type="text" name="login">
-    mot de pass: <input type="password" name="pass">
-    <input type="submit" value="ok">
+
+<form method="post" action="valider.php">
+  <div class="imgcontainer">
+    <img src="img\img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label><br>
+    <input type="text" placeholder="Enter Username" name="login" required><br>
+
+    <label for="psw"><b>Password</b></label><br>
+    <input type="password" placeholder="Enter Password" name="pass" required>
+        
+    <button type="submit">Login</button>
+  </div>
+
 </form>
 
 </body>
-    </html>
-    
+</html>
